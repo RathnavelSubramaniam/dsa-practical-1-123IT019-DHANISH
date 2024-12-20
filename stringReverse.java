bce81ccf70a200140ec3688497232d4ae9217be9
123IT019-DHANISH <139448217+123IT019-DHANISH@users.noreply.github.com>
2024-12-20 09:24:32 +0000

    done

--- stringReverse.java
+++ stringReverse.java
@@ -8,12 +8,18 @@ System.out.println("ENTER THE STRING TO BE REVERSED USING RECURSION");
 Scanner input = new Scanner(System.in);
 String str = input.nextLine();
 String reversed="";
-System.out.println("THE REVERSED STRING IS: " +reverseString(str,"",0,str.length()-1) );
+System.out.println("THE REVERSED STRING IS: " + reverseString(str,"",0,str.length()-1) );
 }
-public static String reverseString()
- {
+public static String reverseString(String str,String reversed,int startIndex,int endIndex)
 
-//TYPE YOUR CODE HERE
+ {
+    if(startIndex > endIndex)
+    {
+        return reversed;
+    }
+reversed +=str.charAt(endIndex);
+return reverseString(str,reversed,startIndex,endIndex - 1);
+
 
  }
 }
